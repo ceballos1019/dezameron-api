48687807df940f2f1e6a6e5f76ca967be0850c31
Andres Ceballos Sanchez <andresrxn10@hotmail.com>
2017-10-23 21:08:22 -0500

    Added Room model and a service to search rooms

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,23 +1,48 @@
 package controllers
 
 import javax.inject._
+
+import models.Hotel
+import models.Hotel.collection
+import models.Room.rooms
+import org.mongodb.scala.{Document, MongoClient, MongoCollection, MongoDatabase, Observer}
 import play.api._
+import play.api.libs.json.Json
 import play.api.mvc._
-
+import models.Helpers._
+import org.mongodb.scala.model.Filters._
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc)
+{
 
+  def test = Action{
+    Ok(Json.toJson(collection.find().results()))
+  }
+
+  //http://localhost:9000/v1/rooms?arrive_date=hola&leave_date=chao&city=05001&hosts=3&room_type=L
+  def search(arrive_date: String, leave_date: String, city:String,
+             hosts: Int, room_type:String)  =
+    Action{
+      Ok(Json.toJson(rooms.find(and(equal("city", city), equal("capacity",hosts),
+        equal("room_type",room_type))).results()))
+    }
+
+  def insert = Action{
+    val hotel: Hotel = Hotel(8, "InsertTest4")
+    collection.insertOne(hotel).results()
+    Ok("Ingresado papo")
+  }
   /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+    * Create an Action to render an HTML page.
+    *
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
---
+++ app/models/Hotel.scala
@@ -0,0 +1,34 @@
+package models
+
+
+import org.mongodb.scala.bson.ObjectId
+import org.mongodb.scala.bson.codecs.Macros._
+import org.mongodb.scala.bson.codecs.DEFAULT_CODEC_REGISTRY
+import org.bson.codecs.configuration.CodecRegistries.{fromProviders, fromRegistries}
+import org.mongodb.scala.{Completed, Document, MongoClient, MongoCollection, MongoDatabase, Observer}
+import play.api.libs.json._
+import models.Helpers._
+
+case class  Hotel(_id: ObjectId, hotel_id: Double, hotel_name: String)
+
+object Hotel {
+
+  def apply(hotel_id: Double, hotel_name: String): Hotel =
+    Hotel(new ObjectId(),hotel_id,hotel_name)
+
+  //Para poder convertir el modelo a BSON y viceversa
+  val codecRegistry = fromRegistries(fromProviders(classOf[Hotel]), DEFAULT_CODEC_REGISTRY )
+  val mongoClient: MongoClient = MongoClient()
+  val database: MongoDatabase = mongoClient.getDatabase("dezamerondb").withCodecRegistry(codecRegistry)
+  val collection: MongoCollection[Hotel] = database.getCollection("hotel")
+
+  implicit val objectIdWrites = new Writes[ObjectId] {
+    def writes(oId: ObjectId): JsValue = {
+      JsString(oId.toString)
+    }
+  }
+
+  implicit val hotelWrite = Json.writes[Hotel]
+  //implicit val hotelRead = Json.reads[Hotel]
+
+}
